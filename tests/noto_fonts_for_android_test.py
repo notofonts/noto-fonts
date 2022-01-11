212e10082d6d40a1a614780b78b841123c6cbafa
Rod S <rsheeter@google.com>
2022-01-10 23:09:02 -0800

    Sample of a weight test

--- tests/noto_fonts_for_android_test.py
+++ tests/noto_fonts_for_android_test.py
@@ -1,6 +1,8 @@
+from fontTools import ttLib
 from lxml import etree
 from pathlib import Path
 import pytest
+from typing import Tuple
 
 
 _KNOWN_PATHLESS = {
@@ -9,6 +11,7 @@ _KNOWN_PATHLESS = {
   "NotoColorEmojiFlags.ttf",
   "NotoSansSymbols-Regular-Subsetted2.ttf",
 }
+
 
 
 def _repo_root() -> Path:
@@ -29,6 +32,44 @@ def _font_file(font_el) -> str:
   return ("".join(font_el.itertext())).strip()
 
 
+def _font_path(font_el) -> Path:
+  name = _font_file(font_el)
+  path = font_el.attrib["path"]
+  return _repo_root() / path / name
+
+
+def _is_collection(font_el) -> bool:
+  return _font_file(font_el).lower().endswith(".ttc")
+
+
+def _open_font(font_el) -> ttLib.TTFont:
+  path = _font_path(font_el)
+  if not path.is_file():
+    raise IOError(f"No such file: {path}")
+
+  if _is_collection(font_el):
+    return ttLib.TTFont(str(path), fontNumber=int(font_el.attrib["index"]))
+  return ttLib.TTFont(str(path))
+
+
+def _axis(font, tag):
+  if "fvar" not in font:
+    return None
+  axes = tuple(a for a in font["fvar"].axes if a.axisTag == tag)
+  if not axes:
+    return None
+  assert len(axes) < 2, f"only 0 or 1 fvar entries supported; {tag} has more"
+  return axes[0]
+
+
+def _weight(font: ttLib.TTFont) -> Tuple[int, int, int]:
+  maybe_wght = _axis(font, "wght")
+  if maybe_wght:
+    return (maybe_wght.minValue, maybe_wght.defaultValue, maybe_wght.maxValue)
+  os2_weight = font["OS/2"].usWeightClass
+  return (os2_weight, os2_weight, os2_weight)
+
+
 def test_fonts_have_path():
   root = etree.parse(str(_noto_4_android_file()))
   bad = []
@@ -43,12 +84,42 @@ def test_fonts_have_path():
   assert not bad, "Missing path attribute: " + ", ".join(bad)
 
 
+def test_ttcs_have_index():
+  root = etree.parse(str(_noto_4_android_file()))
+  bad = []
+  for font in root.iter("font"):
+    if not _is_collection(font):
+      continue
+    if "index" not in font.attrib:
+      bad.append(_font_file(font))
+  assert not bad, "Missing index attribute: " + ", ".join(bad)
+
+
 def test_font_paths_are_valid():
   root = etree.parse(str(_noto_4_android_file()))
   bad = []
   for font in root.xpath("//font[@path]"):
-    path = font.attrib["path"]
-    file = _font_file(font)
-    if not (_repo_root() / path / file).is_file():
-      bad.append("/".join((path, file)))
+    path = _font_path(font)
+    if not path.is_file():
+      bad.append(str(path))
   assert not bad, "No such file: " + ", ".join(bad)
+
+
+def test_font_weights():
+  root = etree.parse(str(_noto_4_android_file()))
+  errors = []
+  for font_el in root.xpath("//font[@path]"):
+    xml_weight = int(font_el.attrib["weight"])
+    path = _font_path(font_el)
+
+    font = _open_font(font_el)
+    min_wght, default_wght, max_weight = _weight(font)
+
+    if xml_weight < min_wght or xml_weight > max_weight:
+      errors.append(f"{_font_file(font_el)} weight {xml_weight} outside font capability {min_wght}..{max_weight}")
+
+  assert not errors, ", ".join(errors)
+
+
+def test_font_psnames():
+  pass
