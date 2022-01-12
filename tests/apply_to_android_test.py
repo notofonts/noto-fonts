d03da67a39ea04759000ea10c96e5de6e3d1b7ae
Rod S <rsheeter@google.com>
2022-01-11 22:39:05 -0800

    WIP

--- tests/apply_to_android_test.py
+++ tests/apply_to_android_test.py
@@ -1,4 +1,5 @@
-from android_connection import apply_to_android
+from android_connection.apply_to_android import _validate_android_path
+from lxml import etree
 from pathlib import Path
 import pytest
 
@@ -11,10 +12,11 @@ def _fake_android_dir() -> Path:
   return _testdata_dir() / "fake_android"
 
 
-def test_apply_to_bad_dir():
-  with pytest.raises(ValueError, match="not found"):
-    apply_to_android._validate_android_path(_testdata_dir())
+def test_validate_bad_dir():
+  with pytest.raises(ValueError, match="missing"):
+    _validate_android_path(_testdata_dir())
 
 
-def test_apply_to_good_dir():
-  apply_to_android._validate_android_path(_fake_android_dir())
+def test_validate_good_dir():
+  _validate_android_path(_fake_android_dir())
+
